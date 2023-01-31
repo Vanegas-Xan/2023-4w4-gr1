80aa55d60bbaa04cf9941adcb1d1cbf857e12421
Vanegas Bustamante <e1872649@cmaisonneuve.qc.ca>
2023-01-31 11:50:46 -0500

    s2c1 Correction des fichiers style.scss et functions.php

--- functions.php
+++ functions.php
@@ -1,8 +1,6 @@
 <?php 
      function enfiler_css (){
-
-    
-    wp_enqueue_style(      'style-principale-css', //identificateur
+    wp_enqueue_style(      '4w4-gr1-principal', //identificateur
                             get_template_directory_uri() . '/style.css', //adresse url
                             array(),//définir les dépendances
                             filemtime(get_template_directory() . '/style.css'), // le calcul de la version du feuille de style
