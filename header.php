37c337b1aded430732261540b6ac498371e7e6cf
Vanegas Bustamante <e1872649@cmaisonneuve.qc.ca>
2023-02-09 11:38:04 -0500

    s3c2 Ajout du style display grid et du fichier documentation

--- header.php
+++ header.php
@@ -9,24 +9,25 @@
 <?php wp_head(); ?>
 
 <body <?php body_class("site");?>>
-<?php the_custom_logo(); ?>
+
 <header class="site__entete">
 <a class = "site__titre" href="<?= bloginfo('url') ?>"><h3><h1><?= bloginfo('name') ?></h1></a>
 <h2 class="site__description"><?= bloginfo('description') ?></h2>
-</header >
-  <?php wp_nav_menu(array (
+<?php the_custom_logo(); ?>
+<?php wp_nav_menu(array (
                          "menu" =>  "entete",
                          "container" =>  "nav",
                          "container_class" =>  "menu__entete"
 
 )); ?>
+</header >
+ 
 
 
     <main class="principal">
     <h1>4W4-Création d'interface et développement <?php ?></h1>
     
     </main>
-    <header >
-    </header>
-</body>
+    
+
 </html>
