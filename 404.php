df135d42705edf16faecc5d40a2347507f065d18
Vanegas Bustamante <e1872649@cmaisonneuve.qc.ca>
2023-03-30 09:36:23 -0400

    s9c2 Ajout d'une fonction pour enlever le aside et de  titres

--- 404.php
+++ 404.php
@@ -5,9 +5,23 @@
  */
 ?>
 <?php get_header(); ?>
+
+<?php
+$nouvel_class = "";
+if (is_front_page()){
+    $nouvel_class = "no-aside";
+}
+
+?>
  <main>
   <h3>404.php</h3>
-  <h1>Page non disponible</h1>
+  <h1 class="titre__404">Page introuvable, vous pouvez une recherche</h1>
+   <div class="cours_404">
+     <h1>Les choix de cours</h1>
+   </div>
+    <div class="notes_4w4">
+    <h1>Notes de cours</h1>
+    </div>
 </main>
 
 <?php get_footer(); ?>
