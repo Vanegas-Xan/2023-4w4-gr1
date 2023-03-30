63994704b99381fd049dce2f70be8967a9908162
Vanegas Bustamante <e1872649@cmaisonneuve.qc.ca>
2023-03-30 11:06:46 -0400

    s9c2 : Création de 404.scss et ajout de style pour modifier la structure de 404.php

--- 404.php
+++ 404.php
@@ -6,16 +6,17 @@
 ?>
 <?php get_header(); ?>
 
- <main >
-  <h3>404.php</h3>
-  <h1 class="titre__404">Page introuvable, vous pouvez une recherche</h1>
+ <main class="site__404" >
+ 
+  <h1 class="titre__404">Erreur 404</h1>
+  <h2 class="titre__404">Page introuvable, vous pouvez une recherche</h2>
 
       <div class="recherche_404">
          <?= get_search_form(); ?>
       </div>
                    
        <div class="cours_404">
-         <h1>Les choix de cours</h1>
+         <h2>Les choix de cours</h2>
          <?php
           $menu = "4w4";
          wp_nav_menu(array (             
@@ -26,7 +27,7 @@
        ?>
       </div>
       <div class="notes_4w4">
-        <h1>Notes de cours</h1>
+        <h2>Notes de cours</h2>
         <?php
          $menu = "cours";
          wp_nav_menu(array (             
