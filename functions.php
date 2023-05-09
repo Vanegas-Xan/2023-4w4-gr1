0fddb376a076f8bcf9f2212d72ff51363327170f
Vanegas Bustamante <e1872649@cmaisonneuve.qc.ca>
2023-05-09 12:04:45 -0400

    s15 c1 Ajout de add_menu_description_and_post-thumbnail sur function.php

--- functions.php
+++ functions.php
@@ -103,6 +103,23 @@ if($args->menu == '4w4'){
 add_filter('nav_menu_item_title', 'perso_menu_item_title', 10, 3);
 
 }
+/** Description et l'image mise en avant dans la page evenement */
+
+function add_menu_description_and_thumbnail( $item_output, $item, $depth, $args ) {
+  if ( 'evenement' == $args->menu) {
+      $post_thumbnail_id = get_post_thumbnail_id( $item->object_id );
+      if ( $post_thumbnail_id ) {
+          $post_thumbnail_url = wp_get_attachment_image_src( $post_thumbnail_id, 'thumbnail' );
+          $item_output = str_replace( '">' . $args->link_before . $item->title, '">' . $args->link_before . '<span class="title">' . $item->title . '</span><span class="description">' . $item->description . '</span><img src="' . esc_url( $post_thumbnail_url[0] ) . '" class="menu-thumbnail" />', $item_output );
+      } else {
+          $item_output = str_replace( '">' . $args->link_before . $item->title, '">' . $args->link_before . '<span class="title">' . $item->title . '</span><span class="description">' . $item->description . '</span>', $item_output );
+      }
+  }
+  return $item_output;
+}
+add_filter( 'walker_nav_menu_start_el', 'add_menu_description_and_thumbnail', 10, 4 );
+
+
   /**------------------------------------------------------------------ Enregistrement des widgets */
   function enregistrer_sidebar() {
     register_sidebar( array(
