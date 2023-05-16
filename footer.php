9450d844ed775f6de566290b5930c79ce3fa7408
Vanegas Bustamante <e1872649@cmaisonneuve.qc.ca>
2023-05-16 09:37:59 -0400

    s16 c1 Ajustement  de la structure de single.php

--- footer.php
+++ footer.php
@@ -1,18 +1,20 @@
 <footer class="site__footer">
-<section class="footer__col"></section>
+  <section class="sidebar__footer">
+<section class="footer__col">
     <div class="sidebar">
       <?php dynamic_sidebar( 'footer_1' ); ?>
     </div>
 </section>
-<section class="footer__col"></section>
+<section class="footer__col">
     <div class="sidebar">
       <?php dynamic_sidebar( 'footer_2' ); ?>
     </div>
 </section>
-<section class="footer__col"></section>
+<section class="footer__col">
     <div class="sidebar">
       <?php dynamic_sidebar( 'footer_3' ); ?>
     </div>
+</section>
 </section>
     <h3>le pied de page</h3>
 
